f342c8831ef077271ab11db9d76553364af1cd4b
JuanServin <juan-servin@hotmail.com>
2020-09-12 17:06:58 -0300

    versão de teste

---
+++ create.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<meta charset="utf-8">
+    <head>
+        <title>Logon</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+<body>
+<div id="formbody">
+    <h1>Create account</h1>
+    <form method="POST">
+        <input  name="user" type="text" placeholder="Username">
+        <input  name="e-mail" type="e-mail" placeholder="e-mail">
+        <input  name="password" type="password" placeholder="Password">
+        <input  name="password" type="password" placeholder="confirm password">
+        <input  name="logon" value="Logon" type="submit">
+        <a href="index.php">already have an account?<strong> Log in here!</strong> 
+    </form>
+</div>
+
+<?php 
+
+    
+
+?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<meta charset="utf-8">
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+<body>
+<div id="formbody">
+    <h1>Welcome!</h1>
+    <form method="POST">
+        <input  name="user" type="text" placeholder="Username">
+        <input  name="password" type="password" placeholder="Password">
+        <input  name="login" value="Login" type="submit">
+        <a href="create.php">New here? <strong>Log on now!</strong> 
+    </form>
+</div>
+
+<?php 
+
+
+
+?>
+
+</body>
+</html>
